e4666d0a83979741788f011046f8ecf60ec7e066
Abhishekp121 <124229895+Abhishekp121@users.noreply.github.com>
2026-02-23 00:08:03 +0530

    axios store and protectesRoute unauthrized authSlice file

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  theme: {
+    extend: {
+      colors: {
+        primary: "rgb(var(--color-primary) / <alpha-value>)",
+        secondary: "rgb(var(--color-secondary) / <alpha-value>)",
+        background: "rgb(var(--color-background) / <alpha-value>)",
+        text: "rgb(var(--color-text) / <alpha-value>)",
+      },
+    },
+  },
+};
